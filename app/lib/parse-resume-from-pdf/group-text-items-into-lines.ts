af0933a7da13518a630efe2e6698fd09a9fab031
akram zayane <akremzayen@gmail.com>
2024-03-28 15:23:04 +0100

    group-text-into-lines Done

--- app/lib/parse-resume-from-pdf/group-text-items-into-lines.ts
+++ app/lib/parse-resume-from-pdf/group-text-items-into-lines.ts
@@ -1,65 +1,105 @@
-import { text } from "stream/consumers";
-import { TextItems, lines,line } from "./types";
+import { line, lines, TextItems } from "./types";
 
-export const groupTextItemsIntoLines=(textItems : TextItems):lines =>{
-    const lines : lines =[];
-    let line:line=[];
+export const groupTextItemsIntoLines = (textItems: TextItems): lines => {
+  const lines: lines = [];
 
-    for(let item of textItems){
-        if(item.hasEndOfLine){
-            if(item.text.trim() !==""){
-                line.push({...item});
-            }
-            lines.push(line);
-            line=[];
-        }
-        else if(item.text.trim() !==""){
-            line.push({...item});
-        }
+  let line: line = [];
+  for (let item of textItems) {
+    if (item.hasEndOfLine) {
+      if (item.text.trim() !== "") {
+        line.push({ ...item });
+      }
+      lines.push(line);
+      line = [];
+    } else if (item.text.trim() !== "") {
+      line.push({ ...item });
     }
-    if(line.length>0){
-        lines.push(line);
+  }
+  if (line.length > 0) {
+    lines.push(line);
+  }
+
+  const typicalCharWidth = getTypicalCharWidth(lines.flat());
+
+  for (let line of lines) {
+    for (let i = line.length - 1; i > 0; i--) {
+      const currentItem = line[i];
+      const leftItem = line[i - 1];
+      const leftItemXEnd = leftItem.x + leftItem.width;
+      const distance = currentItem.x - leftItemXEnd;
+      if (distance <= typicalCharWidth) {
+        if (shouldAddSpaceBetweenText(leftItem.text, currentItem.text)) {
+          leftItem.text += " ";
+        }
+        leftItem.text += currentItem.text;
+
+        const currentItemXEnd = currentItem.x + currentItem.width;
+        leftItem.width = currentItemXEnd - leftItem.x;
+        line.splice(i, 1);
+      }
     }
+  }
 
-    return lines;
+  return lines;
+};
 
-       
-}
+const shouldAddSpaceBetweenText = (leftText: string, rightText: string) => {
+  const leftTextEnd = leftText[leftText.length - 1];
+  const rightTextStart = rightText[0];
+  const conditions = [
+    [":", ",", "|", "."].includes(leftTextEnd) && rightTextStart !== " ",
+    leftTextEnd !== " " && ["|"].includes(rightTextStart),
+  ];
 
-const getTypicalCharacterWidth = (textItems: TextItems):number => {
-    //exclude empty space in the textItems
-    textItems=textItems.filter((item) =>item.text.trim() !=="");
+  return conditions.some((condition) => condition);
+};
 
-const heightToCount:{[height:number]:number}={};
-let commonHeight=0;
-let heightMaxCount=0
+const getTypicalCharWidth = (textItems: TextItems): number => {
+  textItems = textItems.filter((item) => item.text.trim() !== "");
 
-const fontNameToCount:{[fontName:string]:number}={}
-let commonFontName="";
-let fontNameMaxCount=0;
+  const heightToCount: { [height: number]: number } = {};
+  let commonHeight = 0;
+  let heightMaxCount = 0;
 
-for (let item of textItems){
-    const {fontName, width, height}=item;
-    if(!heightToCount[height]){
-        heightToCount[height]=0;
+  const fontNameToCount: { [fontName: string]: number } = {};
+  let commonFontName = "";
+  let fontNameMaxCount = 0;
+
+  for (let item of textItems) {
+    const { text, height, fontName } = item;
+
+    if (!heightToCount[height]) {
+      heightToCount[height] = 0;
     }
     heightToCount[height]++;
-    if(heightToCount[height]> heightMaxCount){
-        commonHeight=height;
-        heightMaxCount=heightToCount[height]
+    if (heightToCount[height] > heightMaxCount) {
+      commonHeight = height;
+      heightMaxCount = heightToCount[height];
     }
 
-
-    if(!fontNameToCount[fontName]){
-        fontNameToCount[fontName]=0;
+    if (!fontNameToCount[fontName]) {
+      fontNameToCount[fontName] = 0;
     }
-   
-    fontNameToCount[fontName]+=text.length
-    if(fontNameToCount[fontName]>fontNameMaxCount){
-        commonFontName=fontName;
-        fontNameMaxCount=fontNameToCount[fontName]
-
+    fontNameToCount[fontName] += text.length;
+    if (fontNameToCount[fontName] > fontNameMaxCount) {
+      commonFontName = fontName;
+      fontNameMaxCount = fontNameToCount[fontName];
     }
-}
+  }
 
-}
+  const commonTextItems = textItems.filter(
+    (item) => item.fontName === commonFontName && item.height === commonHeight
+  );
+
+  const [totalWidth, numChars] = commonTextItems.reduce(
+    (acc, cur) => {
+      const [preWidth, prevChars] = acc;
+      return [preWidth + cur.width, prevChars + cur.text.length];
+    },
+    [0, 0]
+  );
+
+  const typicalCharWidth = totalWidth / numChars;
+
+  return typicalCharWidth;
+};
--- app/lib/parse-resume-from-pdf/index.ts
+++ app/lib/parse-resume-from-pdf/index.ts
@@ -1,3 +1,4 @@
+import { groupTextItemsIntoLines } from "./group-text-items-into-lines"
 import { readPdf } from "./read-pdf"
 
 export const parseResumeFromPdf = async(fileUrl:string) => {
@@ -5,5 +6,6 @@ export const parseResumeFromPdf = async(fileUrl:string) => {
     const textItems = await readPdf(fileUrl)
 
     //Second step is group TEXT ITEMS INTO LINES
-    
+    const lines = groupTextItemsIntoLines(textItems)
+
 }
