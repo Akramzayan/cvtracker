772c489a0370a163fc398b9bb53f5d3a958ddfa1
akram zayane <akremzayen@gmail.com>
2024-05-28 16:27:10 +0200

    AuthV5 starting initital Prisma Done && prisma adapter Db && connecting to db  && auth && route&& middleware

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+import { handlers } from "@/app/auth"; // Referring to the auth.ts file we just created  in the app folder 
+export const {GET,POST} = handlers; // Exporting the handlers object from the auth.ts file 
---
+++ app/middleware.ts
@@ -0,0 +1,1 @@
+export {auth as middleware} from "@/app/auth"; // Exporting the auth object from the auth.ts file
