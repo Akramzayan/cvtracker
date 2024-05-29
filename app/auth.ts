40faac09883ea4e81e5ab67a89de69ef7092d2a7
akram zayane <akremzayen@gmail.com>
2024-05-29 13:50:35 +0200

    google0auth Done

--- app/auth.ts
+++ app/auth.ts
@@ -1,8 +1,17 @@
 import NextAuth from "next-auth"
 import {PrismaAdapter} from "@auth/prisma-adapter"
 import prisma from "./lib/prisma"
+import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme:{
+    logo:"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+    brandColor:"#4285F4",
+    colorScheme:"auto",
+    
+  
+
+  },
     adapter:PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google],
 })
